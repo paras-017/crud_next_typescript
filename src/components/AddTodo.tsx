8df99f55fcc15b9849769e9dcfea5a6681d71a68
paras-017 <paraschauhan0017@gmail.com>
2023-09-20 18:46:22 +0530

    input and add button functionaly added

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,18 @@
+import React, { FormEvent, useState } from 'react'
+
+const AddTodo = () => {
+  const [todo, setTodo] = useState('')
+  const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
+     e.preventDefault()
+    //  handleAddTodo(todo);
+     setTodo('')
+  }
+  return (
+   <form onSubmit={handleFormSubmit}>
+    <input type="text" placeholder='Write your todo' name='' value={todo} onChange={(e)=>setTodo(e.target.value)}/>
+    <button type='submit'>Add</button>
+   </form>
+  )
+}
+
+export default AddTodo
